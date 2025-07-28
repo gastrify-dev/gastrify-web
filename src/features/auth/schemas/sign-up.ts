8d2048c8de85c5b69f8cd7378203fc25777f7d28
David Aragundy <waldaara@espol.edu.ec>
2025-07-28 15:18:23 -0500

    ⬆️ Zod v4

--- src/features/auth/schemas/sign-up.ts
+++ src/features/auth/schemas/sign-up.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 
 import { isValidIdentificationNumber } from "@/shared/utils/is-valid-identification-number";
 
--- src/features/settings/schemas/update-notifications.ts
+++ src/features/settings/schemas/update-notifications.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 
 export const updateNotificationsSchema = z.object({
   type: z.enum(["all", "mentions", "none"], {
